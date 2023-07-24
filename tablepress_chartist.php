85045aa8c709bebeb7e6b692197f125fd5443294
Onur Can Karaca <wavesnshapes@gmail.com>
2023-07-23 19:30:17 -0600

    made so the default table high auto value * 1.05 and other fixes

--- tablepress_chartist.php
+++ tablepress_chartist.php
@@ -162,17 +162,19 @@ class TablePress_Chartist
      * @since 0.1
      */
     public static function enqueue_scripts_styles()
-    {
-        global $post;
-        if (is_a($post, 'WP_Post') && has_shortcode($post->post_content, 'table-chart')) {
-            $dir = plugin_dir_url(__FILE__);
-            wp_enqueue_script('chartist-js', $dir.'libdist/chartist.min.js', ['jquery'], self::$version, true);
-            wp_enqueue_style('chartist-css', $dir.'libdist/chartist.min.css', [], self::$version);
-            if (file_exists(WP_CONTENT_DIR.'/tablepress-chartist-custom.css')) {
-                wp_enqueue_style('chartist-custom-css', content_url('tablepress-chartist-custom.css'), ['chartist-css'], self::$version);
-            }
+	{
+    global $post;
+    if (is_a($post, 'WP_Post') && has_shortcode($post->post_content, 'table-chart')) {
+        $dir = plugin_dir_url(__FILE__);
+        wp_enqueue_script('chartist-js', $dir.'libdist/chartist.min.js', ['jquery'], self::$version, true);
+        wp_enqueue_script('chartist-tooltip-plugin', $dir.'libdist/chartist-plugin-tooltip.min.js', ['chartist-js'], self::$version, true); // Add this line
+        wp_enqueue_style('chartist-css', $dir.'libdist/chartist.min.css', [], self::$version);
+        wp_enqueue_style('chartist-tooltip-css', $dir.'libdist/chartist-plugin-tooltip.css', ['chartist-css'], self::$version); // And this line
+        if (file_exists(WP_CONTENT_DIR.'/tablepress-chartist-custom.css')) {
+            wp_enqueue_style('chartist-custom-css', content_url('tablepress-chartist-custom.css'), ['chartist-css'], self::$version);
         }
     }
+	}
 
     /**
      * Add the Extension's parameters as valid [table /] Shortcode attributes.
@@ -205,56 +207,54 @@ class TablePress_Chartist
      * @return string The generated HTML and JavaScript code for the chart.
      */
     public static function generate_chart($output, $table, $render_options)
-    {
-        if (!$render_options['chartist']) {
-            return $output;
-        }
+{
+    if (!$render_options['chartist']) {
+        return $output;
+    }
 
-        $json_chart_options = [];
+    $json_chart_options = [];
 
-        // Determine/sanitize the chart type and add JS calculation functions.
-        switch (strtolower($render_options['chartist_chart'])) {
-            case 'bar':
-                $chart = 'Bar';
-                break;
-            case 'pie':
-                $chart = 'Pie';
-                $json_chart_options[] = 'labelInterpolationFnc: function( value ) { return value; }';
-                break;
-            case 'donut':
-                $chart = 'Pie';
-                $json_chart_options[] = 'labelInterpolationFnc: function( value ) { return value; }';
-                $json_chart_options[] = 'donut: true';
-                break;
-            case 'percent':
-                $chart = 'Pie';
-                $json_chart_options[] = "labelInterpolationFnc: function( value ) { return Math.round( value / data.series.reduce( sum ) * 100 ) + '%'; }";
-                break;
-            case 'piepercent':
-                $chart = 'Pie';
-                $json_chart_options[] = "labelInterpolationFnc: function( value, index ) { return value + ' (' + Math.round(data.series[index] / data.series.reduce( sum ) * 100) + '%)';}";
-                break;
-            case 'donutpercent':
-                $chart = 'Pie';
-                $json_chart_options[] = "labelInterpolationFnc: function( value, index ) { return value + ' (' + Math.round(data.series[index] / data.series.reduce( sum ) * 100) + '%)';}";
-                $json_chart_options[] = 'donut: true';
-                break;
-            case 'line':
-            default:
-                $chart = 'Line';
-                break;
-        }
+    switch (strtolower($render_options['chartist_chart'])) {
+        case 'bar':
+            $chart = 'Bar';
+            break;
+        case 'pie':
+            $chart = 'Pie';
+            $json_chart_options[] = 'labelInterpolationFnc: function( value ) { return value; }';
+            break;
+        case 'donut':
+            $chart = 'Pie';
+            $json_chart_options[] = 'labelInterpolationFnc: function( value ) { return value; }';
+            $json_chart_options[] = 'donut: true';
+            break;
+        case 'percent':
+            $chart = 'Pie';
+            $json_chart_options[] = "labelInterpolationFnc: function( value ) { return Math.round( value / data.series.reduce( sum ) * 100 ) + '%'; }";
+            break;
+        case 'piepercent':
+            $chart = 'Pie';
+            $json_chart_options[] = "labelInterpolationFnc: function( value, index ) { return value + ' (' + Math.round(data.series[index] / data.series.reduce( sum ) * 100) + '%)';}";
+            break;
+        case 'donutpercent':
+            $chart = 'Pie';
+            $json_chart_options[] = "labelInterpolationFnc: function( value, index ) { return value + ' (' + Math.round(data.series[index] / data.series.reduce( sum ) * 100) + '%)';}";
+            $json_chart_options[] = 'donut: true';
+            break;
+        case 'line':
+        default:
+            $chart = 'Line';
+            break;
+    }
 
-        // animation frame
-        $animation_script = <<<'JS'
+    $animation_script = <<<'JS'
 chart.on('draw', function(data) {
 	%s
 });
 JS;
-        // Setup animation for chart
-        switch (strtolower($render_options['chartist_animation'])) {
-            case 'buildup':
-                $animation = <<<'JS'
+
+    switch (strtolower($render_options['chartist_animation'])) {
+        case 'buildup':
+            $animation = <<<'JS'
 if(data.type === 'line' || data.type === 'area') {
 	data.element.animate({
 		d: {
@@ -294,77 +294,159 @@ if (data.type === 'bar') {
 	}
 }
 JS;
-                $animation_script = sprintf($animation_script, $animation);
-                break;
+            $animation_script = sprintf($animation_script, $animation);
+            break;
 
-            default:
-                $animation_script = '';
-        }
+        default:
+            $animation_script = '';
+    }
 
-        // Convert all numeric table cell values to numeric variables, so that they show up as numbers in the JSON encoded string, as ChartistJS requires that.
-        foreach ($table['data'] as $row_idx => $row) {
-            foreach ($row as $col_idx => $cell) {
-                $table['data'][$row_idx][$col_idx] = self::_maybe_string_to_number($cell);
+    $maxValue = null;
+    foreach ($table['data'] as $row_idx => $row) {
+        foreach ($row as $col_idx => $cell) {
+            $numericValue = self::_maybe_string_to_number($cell);
+            $table['data'][$row_idx][$col_idx] = $numericValue;
+            if (is_numeric($numericValue) && ($maxValue === null || $numericValue > $maxValue)) {
+                $maxValue = $numericValue;
             }
         }
+    }
 
-        // Get labels from the first table row.
-        if ($render_options['table_head']) {
-            $json_labels = array_shift($table['data']);
+    if ($render_options['table_head']) {
+        $json_labels = array_shift($table['data']);
+    }
+
+    if ('Pie' === $chart) {
+        $table['data'] = array_shift($table['data']);
+    }
+
+    $json_chart_data = [
+        'series' => $table['data'],
+    ];
+    if ($render_options['table_head'] && 'percent' !== $render_options['chartist_chart']) {
+        $json_chart_data['labels'] = $json_labels;
+    }
+
+    $json_chart_data = json_encode((object) $json_chart_data);
+
+    foreach (self::$attribute_to_js_mapping as $option_key => $option_js) {
+        $option_key = 'chartist_'.$option_key;
+        if (isset($render_options[$option_key])) {
+            $value = self::_maybe_string_to_number($render_options[$option_key]);
+            $json_chart_options[] = $option_js.': '.json_encode($value);
         }
+    }
 
-        // Use only the first row in Pie charts.
-        if ('Pie' === $chart) {
-            $table['data'] = array_shift($table['data']);
-        }
-
-        // Create JSON object for the chart data.
-        $json_chart_data = [
-            'series' => $table['data'],
-        ];
-        if ($render_options['table_head'] && 'percent' !== $render_options['chartist_chart']) {
-            $json_chart_data['labels'] = $json_labels;
-        }
-
-        $json_chart_data = json_encode((object) $json_chart_data);
-
-        // Add other chart options.
-        foreach (self::$attribute_to_js_mapping as $option_key => $option_js) {
-            $option_key = 'chartist_'.$option_key;
-            if (isset($render_options[$option_key])) {
-                $value = self::_maybe_string_to_number($render_options[$option_key]);
-                $json_chart_options[] = $option_js.': '.json_encode($value);
+    // Find the maximum value in the table data
+    $maxVal = 0;
+    foreach ($table['data'] as $row) {
+        foreach ($row as $cell) {
+            if (is_numeric($cell) && $cell > $maxVal) {
+                $maxVal = $cell;
             }
         }
-        $json_chart_options = '{ '.implode(', ', $json_chart_options).' }';
+    }
+	// Increase the maximum value by 5%
+    $maxVal *= 1.05;
 
-        // Sanitize the aspect ratio.
-        $aspect_ratio = 'ct-perfect-fourth';
-        if (isset(self::$aspect_ratios[$render_options['chartist_aspect_ratio']])) {
-            $aspect_ratio = self::$aspect_ratios[$render_options['chartist_aspect_ratio']];
-        }
+    // Set the 'high' option to the new maximum value
+    $json_chart_options[] = 'high: ' . $maxVal;
 
-        $chartist_script = <<<JS
+    $json_chart_options[] = 'plugins: [Chartist.plugins.tooltip()]';
+    $json_chart_options = '{ '.implode(', ', $json_chart_options).' }';
+
+    $aspect_ratio = 'ct-perfect-fourth';
+    if (isset(self::$aspect_ratios[$render_options['chartist_aspect_ratio']])) {
+        $aspect_ratio = self::$aspect_ratios[$render_options['chartist_aspect_ratio']];
+    }
+
+    $chartist_script = <<<JS
 <script type="text/javascript">
 jQuery(document).ready(function(){
 	var	data = {$json_chart_data},
 		options = {$json_chart_options},
 		sum = function( a, b ) { return a + b; };
 	var chart = new Chartist.{$chart}( '#chartist-{$render_options['html_id']}', data, options );
+	chart.on('draw', function(data) {
+    if(data.type === 'line' || data.type === 'area') {
+        data.element.animate({
+            d: {
+                begin: 1500 * data.index,
+                dur: 1500,
+                from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+                to: data.path.clone().stringify(),
+                easing: Chartist.Svg.Easing.easeOutQuint
+            }
+        });
+    }
+    if (data.type === 'bar') {
+        data.element.attr({
+            style: 'stroke-width: 0px'
+        });
+        var strokeWidth = 10;
+
+        for (var s = 0; s < data.series.length; ++s) {
+            if (data.seriesIndex === s) {
+                data.element.animate({
+                    y2:             {
+                        begin:  s * 1500,
+                        dur:    1500,
+                        from:   data.y1,
+                        to:     data.y2,
+                        easing: Chartist.Svg.Easing.easeOutSine
+                    },
+                    'stroke-width': {
+                        begin: s * 1500,
+                        dur:   1,
+                        from:  0,
+                        to:    strokeWidth,
+                        fill:  'freeze'
+                    }
+                }, false);
+            }
+        }
+    }
+    if(data.type === 'point') {
+    data.element._node.onmouseover = function() {
+        var tooltip = document.createElement('div');
+        tooltip.className = 'chartist-tooltip';
+        document.body.appendChild(tooltip);
+
+        tooltip.style.left = (event.pageX - 40) + 'px';
+        tooltip.style.top = (event.pageY - 55) + 'px';
+
+        tooltip.style.transition = 'opacity 0.3s ease-in-out';
+
+        tooltip.style.opacity = 1;
+        tooltip.textContent = data.value.y !== undefined ? data.value.y : (data.value.x !== undefined ? data.value.x : data.value);
+
+        data.element._node.tooltip = tooltip;
+    };
+    data.element._node.onmouseout = function() {
+        this.tooltip.style.opacity = 0;
+
+        setTimeout(function() {
+            document.body.removeChild(this.tooltip);
+        }.bind(this), 200);
+    };
+}
+
+});
 
 	{$animation_script}
 });
 </script>
 JS;
 
-        $chartist_divtag = sprintf(
-            "<div id=\"%s\" class=\"ct-chart %s\"></div>\n",
-            "chartist-{$render_options['html_id']}",
-            $aspect_ratio
-        );
+    $chartist_divtag = sprintf(
+        "<div id=\"%s\" class=\"ct-chart %s\"></div>\n",
+        "chartist-{$render_options['html_id']}",
+        $aspect_ratio
+    );
 
-        return $chartist_divtag.$chartist_script;
-    }
+    return $chartist_divtag.$chartist_script;
+}
+
 
     /**
      * Convert a string to int or float, if it's a numeric string.
